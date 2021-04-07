76eb45810cf172984989ce838d359c4db0a55803
Varsion L <48198372+Varsion@users.noreply.github.com>
2021-04-07 09:54:52 +0800

    Version reset.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 		resources :users
 	end
 	
+	namespace :v2 do
+		get 'monitors/version'
+	end
+	
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'homes#index'
 end
