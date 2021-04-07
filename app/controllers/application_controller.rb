2f46d07f950f5bb90b12349e206bd626ce69c575
Varsion L <48198372+Varsion@users.noreply.github.com>
2021-04-07 11:19:30 +0800

    refactor errors handle.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+	rescue_from Exception, with: :all_exception
+	def all_exception exception
+		if Rails.env.production?
+			render json: {status: 10, message: "Some Errors"}, status: :accepted
+		else
+			raise exception
+		end
+	end
+	#
+	def render_success data
+		render json: data, status: :ok
+	end
+	
+	#
+	def render_error code, message
+		render json: {status: code, message: message}
+	end
+	
+	#
+	def render_default_error
+		render_error(20, "Parameter errors")
+	end
 end
