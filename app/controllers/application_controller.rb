61c300dbbb8615e5d8a9720b381b21e4026635cb
Varsion L <48198372+Varsion@users.noreply.github.com>
2021-04-07 14:40:15 +0800

    Add detail errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,23 @@ class ApplicationController < ActionController::API
 			raise exception
 		end
 	end
-	#
+	# success
 	def render_success data
 		render json: data, status: :ok
 	end
 	
-	#
+	# errors
 	def render_error code, message
 		render json: {status: code, message: message}
 	end
 	
-	#
+	# default errors
 	def render_default_error
 		render_error(20, "Parameter errors")
+	end
+	# detail errors
+	# 通过简要错误信息和详细错误信息来进行信息分割
+	def render_detail_error code,message,detail
+		render json: {status: code, message: message, detail: detail}, status: :accepted
 	end
 end
