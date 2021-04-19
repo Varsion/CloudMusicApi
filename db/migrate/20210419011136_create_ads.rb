138e68d803143d31735ba1b876e96fde2a5c494f
Varsion L <48198372+Varsion@users.noreply.github.com>
2021-04-19 15:00:32 +0800

    Rebuild table migrations

--- db/migrate/20210419011136_create_ads.rb
+++ db/migrate/20210419011136_create_ads.rb
@@ -1,13 +1,23 @@
 class CreateAds < ActiveRecord::Migration[5.2]
-  def change
-    create_table :ads do |t|
-      t.string :title
-      t.string :banner
-      t.string :uri
-      t.integer :order
-      t.references :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
+	def change
+		create_table :ads do |t|
+			# 标题
+			t.string :title
+			
+			# 图片地址，相对地址
+			# 要显示时，根据不同的环境拼接不同前缀
+			t.string :banner, null: false
+			
+			# 点击后跳转的url
+			t.string :uri
+			
+			# 排序，值越大，越靠前
+			t.integer :order, null: false, default: 0
+			
+			# 那个用户添加的
+			t.references :user, foreign_key: true
+			
+			t.timestamps
+		end
+	end
 end
--- db/migrate/20210419021828_create_sheets.rb
+++ db/migrate/20210419021828_create_sheets.rb
@@ -1,15 +1,29 @@
 class CreateSheets < ActiveRecord::Migration[5.2]
-  def change
-    create_table :sheets do |t|
-      t.string :title
-      t.string :banner
-      t.string :description
-      t.integer :clicks_count
-      t.integer :collections_count
-      t.integer :comments_count
-      t.references :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
+	def change
+		create_table :sheets do |t|
+			# 歌单标题，不能为空
+			t.string :title, null: false
+			
+			# 封面
+			t.string :banner
+			
+			# 描述
+			t.string :description
+			
+			# 点击率（点击到详情）
+			# 原版是播放音乐量
+			t.integer :clicks_count,limit:8
+			
+			# 收藏数
+			t.integer :collections_count,limit:8
+			
+			# 评论数
+			t.integer :comments_count,limit:8
+			
+			# 创建用户
+			t.references :user, foreign_key: true
+			
+			t.timestamps
+		end
+	end
 end
